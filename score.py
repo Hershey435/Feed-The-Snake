214cfc829020610679c8dc7dd53860edb2f458a0
Harshitha Doppalapudi <78091930+Hershey435@users.noreply.github.com>
2021-09-02 10:24:13 +0530

    Added functionality to store the high score

--- score.py
+++ score.py
@@ -6,6 +6,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as f:
+            self.high = int(f.read())
         self.color("white")
         self.penup()
         self.goto(0, 280)
@@ -14,13 +16,21 @@ class Score(Turtle):
         self.goto(0, 270)
 
     def update_score(self):
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 16, "normal"))
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high}", align="center", font=("Courier", 16, "normal"))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over.", align="center", font=("Courier", 20, "normal"))
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("Game Over.", align="center", font=("Courier", 20, "normal"))
+
+    def reset_score(self):
+        if self.score > self.high:
+            with open("data.txt", mode="w") as f:
+                self.high = self.score
+                f.write(f"{self.high}")
+        self.score = 0
+        self.update_score()
 
     def add_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        self.update_score()
